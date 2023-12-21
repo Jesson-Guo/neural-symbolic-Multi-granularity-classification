b09f8d7ca276a1034787a4264aaa63e27167657f
JessonGuo <1421280853@qq.com>
2023-12-21 15:23:56 +0800

    infer

--- src/tot/infer.py
+++ src/tot/infer.py
@@ -1,5 +1,10 @@
+import torch
+import progressbar
+
 from src.tot.tot import ToT
 from src.gpt import GPT
+from utils.conf import is_main_process
+from utils.util import accuracy, reduce_mean
 
 
 def solve(model, dataloader, node_dict, label_to_wnid, label_to_id, gpt: GPT, tot: ToT):
@@ -13,10 +18,24 @@ def solve(model, dataloader, node_dict, label_to_wnid, label_to_id, gpt: GPT, to
             inner_nodes[node.layer] = []
         inner_nodes[node.layer].append(node)
 
+    acc = 0
+    if is_main_process():
+        bar = progressbar.ProgressBar(0, len(dataloader))
     for idx, (x, targets) in enumerate(dataloader):
         x = model.forward_features(x)
         x = model.forward_head(x, pre_logits=False)
 
+        pred = torch.zeros(x.shape[0])
         for i in range(x.shape[0]):
             output, _ = tot.solve(x, dataloader.dataset.labels, node_dict, label_to_wnid, gpt, method='bfs')
-            output = label_to_id[output]
+            pred[i] = label_to_id[output]
+
+        acc += pred.eq(targets.data).sum()
+
+        if is_main_process():
+            bar.update(i+1)
+
+    acc = reduce_mean(acc, average=False)
+    acc = acc / len(dataloader.dataset)
+    if is_main_process():
+        print(f'eval acc: {acc.item()}')
