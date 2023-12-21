510cf2da8c52473bf9383045c481d3839d75a1c4
JessonGuo <1421280853@qq.com>
2023-12-21 15:23:18 +0800

    add test FakeGPT

--- src/gpt.py
+++ src/gpt.py
@@ -37,3 +37,35 @@ class GPT(object):
         self.prompt_tokens += completion.usage.prompt_tokens
 
         return outputs
+
+
+class FakeGPT(object):
+    fake_label = '''\n- {label}'''
+    fake_cat = '''Category {num}: {name}{cat}'''
+    fake_plan = '''Plan {num}:{plan}'''
+
+    def __init__(
+        self,
+        client: OpenAI,
+        model: str = 'gpt-3.5-turbo',
+        temperature: float = 0.7,
+        max_tokens: int = 1000
+    ) -> None:
+        self.client = client
+        self.model = model
+        self.temperature = temperature
+        self.max_tokens = max_tokens
+
+    def generate(self, labels, n=1, stop=None):
+        l1 = self.fake_label.format(label=labels[0])
+        l2 = ""
+        for i in range(1, len(labels)):
+            l2 += self.fake_label.format(label=labels[i])
+
+        cat1 = self.fake_cat.format(num=1, name='A', cat=l1)
+        cat2 = self.fake_cat.format(num=2, name='B', cat=l2)
+
+        plan1 = self.fake_plan.format(num=1, plan=cat1)
+        plan2 = self.fake_plan.format(num=2, plan=cat2)
+
+        return [plan1+"\n\n"+plan2]
