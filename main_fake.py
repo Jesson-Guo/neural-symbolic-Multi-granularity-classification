f5d47d70faa5d67d6eb648eb86237654f5904a3c
JessonGuo <1421280853@qq.com>
2023-12-21 15:24:27 +0800

    add test code

--- main_fake.py
+++ main_fake.py
@@ -8,11 +8,10 @@ import torch
 import torch.nn as nn
 import torch.utils.data
 import torch.backends.cudnn as cudnn
-from functools import partial
 
 from src.dataloader import create_train_val_dataloader
 from src.node import build_tree
-from src.gpt import GPT
+from src.gpt import FakeGPT
 from src.tot.infer import solve
 from src.tot.tot import ToT
 from utils import metrics
@@ -44,7 +43,7 @@ def main(args):
     tree, node_dict = build_tree(args, val_loader.dataset.class_to_idx, state_dict[''])
 
     client = openai.OpenAI()
-    gpt = GPT(client, model=args.backend, temperature=args.temperature)
+    gpt = FakeGPT(client, model=args.backend, temperature=args.temperature)
 
     sim_func = getattr(metrics, args.sim)
     plan_func = getattr(metrics, args.plan)
@@ -62,21 +61,21 @@ def main(args):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='tiny imagenet training')
+    parser = argparse.ArgumentParser(description='image classification with gpt')
 
     parser.add_argument('--local_rank', default=os.getenv('LOCAL_RANK', -1), type=int)
     parser.add_argument('--epochs', type=int, default=50, help='number of epochs to train')
     parser.add_argument('--seed', type=int, default=72, help='random seed')
     parser.add_argument('--model', type=str, default='vit_base_patch16_224.orig_in21k_ft_in1k', help='model name')
     parser.add_argument('--hier', type=str, default='./structure_released.xml', help='wordnet structure')
-    parser.add_argument('--ckpt', type=str, default='./mycode/weights/jx_vit_base_p16_224-80ecf9dd.pth', help='path of model checkpoint')
+    parser.add_argument('--ckpt', type=str, default='./weights/jx_vit_base_p16_224-80ecf9dd.pth', help='path of model checkpoint')
     parser.add_argument('--root', type=str, default='/root/autodl-tmp/data', help='dataset path')
     parser.add_argument('--data', type=str, default='imagenet', help='dataset name')
     parser.add_argument('-j', '--workers', type=int, default=4, help='number of data loading workers (default: 4)')
     parser.add_argument('--batch_size', type=int, default=64, help='batch size')
     parser.add_argument('--backend', type=str, default='gpt-3.5-turbo', help='gpt model')
     parser.add_argument('--sim', type=str, default='kl_divergence', help='similarity metrics')
-    parser.add_argument('--plan', type=str, default='dunn_index', help='cluster metrics')
+    parser.add_argument('--plan', type=str, default='silhouette_score', help='cluster metrics')
 
     args = parser.parse_args()
     main(args)
