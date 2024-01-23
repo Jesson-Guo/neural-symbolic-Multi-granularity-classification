bd65721adccbd6f3dcab7ba45befaa2d435f11c0
JessonGuo <1421280853@qq.com>
2024-01-24 02:24:51 +0800

    test cifar100

--- scripts/baseline/run_cifar100-lt_dist.sh
+++ scripts/baseline/run_cifar100-lt_dist.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 python -m torch.distributed.launch --nproc_per_node=2 --master_port 29501 \
         main.py \
-        --devices 0,1 \
-        --epochs 500 \
+        --devices 2,3 \
+        --epochs 300 \
         --lr 0.0005 \
         --method vit \
         --k 10 \
-        --data cifar100 \
+        --data cifar100-lt \
         --words /root/autodl-tmp/data/cifar-100-python/words.txt \
         --config ./src/vpt/configs/files/simple/cifar100-lt.yaml \
         --loss ldam \
